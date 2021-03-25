a6c218980ae0431702ad48e6d2cb177bf0147b5e
fewbadboy <renekton.king@qq.com>
2021-03-25 12:17:44 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// port = 6900 npm run dev OR npm run dev --port = 6900
+const port = process.env.port || 6900 // dev port
+
+module.exports = {
+  /**
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
+   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then publicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+    //  before: require('./mock/mock-server.js')
+  }
+}
